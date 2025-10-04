e04c53f816e20332d1a43413491fb7913df38eb5
zaid <zaidkazmi24@gmail.com>
2025-10-04 18:01:06 +0530

    yes2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = require('./db');
 const idempotency = require('./middleware/idempotency');
 
 const app = express();
-
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
